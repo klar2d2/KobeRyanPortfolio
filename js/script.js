3e4d0ff2841755d61cc82300b3a9451fdcd45077
klar2d2 <koberyan@comcast.net>
2020-03-10 11:17:06 -0700

    image recanvas

--- js/script.js
+++ js/script.js
@@ -23,6 +23,7 @@ function handleClick() {
     centerPara.innerHTML = 'An idea I came up with working at the Seattle Art Museum, this app allows you to look up your favorite art from the MET&#39s archives, collect your favorites into a personal list, and see if they are currently on display. Noteable technologies are Express.js, Node.js and JavaScript.'
     centerImg.src = '../public/met.png'
     centerImg.style = 'visibility:visible'
+    centerImg.style = 'width: 60%'
     centerImg.alt = 'artwork'
     centerLink.href = 'https://met-collection-creator.herokuapp.com/'
   }
@@ -31,6 +32,7 @@ function handleClick() {
     centerPara.innerHTML = 'Inspired by the frustration of games like QWOP and Getting Over It with Bennett Foddy, I implemented a basic shoe tying simulator. Notabele technologies user are Phaser.io, HTML, CSS'
     centerImg.src = '../public/shoolace.png'
     centerImg.style = 'visibility:visible'
+    centerImg.style = 'width: 60%'
     centerImg.alt = 'shoelace simulator'
     centerLink.href = 'https://klar2d2.github.io/shoe-project/'
   }
@@ -39,6 +41,7 @@ function handleClick() {
     centerPara.innerHTML = 'My first group project, Inkline is an app that connects with Facebook Oauth and grabs images off of our user&#39s Instagram to display. Noteable technologies used were TypeScript, Facebook Oauth and Material-UI'
     centerImg.src = '../public/inline.png'
     centerImg.style = 'visibility:visible'
+    centerImg.style = 'width: 80%'
     centerImg.alt = 'Inkline'
     centerLink.href = 'https://inkline-gtol.herokuapp.com/'
   }
@@ -47,6 +50,7 @@ function handleClick() {
     centerPara.innerHTML = 'Originating as a group project to practice Socket.io, Munch My Lawn is a CMS to connect homeowners to goats, skipping the hassle of a middle man farmer. Notable technologies used were React,Socket and SASS.'
     centerImg.src = '../public/munchy.png'
     centerImg.style = 'visibility:visible'
+    centerImg.style = 'width: 80%'
     centerImg.alt = 'Munch My Lawn'
     centerLink.href = 'https://munchmylawn.herokuapp.com/'
   }
